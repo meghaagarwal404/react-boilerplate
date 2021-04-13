1d13fb8749db2c6fa06c49d78aace0f90e7174ca
meghaagarwal404 <meghaagarwal404@gmail.com>
2021-04-13 16:30:05 +0530

    react install

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,13 @@
-'use strict';
+"use strict";
 
-var _web = require('./web.server');
-
-var _web2 = _interopRequireDefault(_web);
+var _web = _interopRequireDefault(require("./web.server"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var webServer = new _web2.default();
-webServer.start().then(function () {
+var webServer = new _web.default();
+webServer.start().then(() => {
   console.log('Web server started!');
-}).catch(function (err) {
+}).catch(err => {
   console.error(err);
   console.error('Failed to start web server');
 });
--- dist/web.server.test.js
+++ dist/web.server.test.js
@@ -1,50 +1,24 @@
-'use strict';
+"use strict";
 
-var _web = require('./web.server');
-
-var _web2 = _interopRequireDefault(_web);
+var _web = _interopRequireDefault(require("./web.server"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-describe('Started', function () {
-    var webServer = null;
-    beforeAll(function () {
-        webServer = new _web2.default();
-    });
-    test('should start and trigger a callback', _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-        var promise;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-            while (1) {
-                switch (_context.prev = _context.next) {
-                    case 0:
-                        promise = webServer.start();
-                        _context.next = 3;
-                        return expect(promise).resolves.toBeUndefined();
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-                    case 3:
-                    case 'end':
-                        return _context.stop();
-                }
-            }
-        }, _callee, undefined);
-    })));
-    test('should stop and trigger a callback', _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
-        var promise;
-        return regeneratorRuntime.wrap(function _callee2$(_context2) {
-            while (1) {
-                switch (_context2.prev = _context2.next) {
-                    case 0:
-                        promise = webServer.stop();
-                        _context2.next = 3;
-                        return expect(promise).resolves.toBeUndefined();
-
-                    case 3:
-                    case 'end':
-                        return _context2.stop();
-                }
-            }
-        }, _callee2, undefined);
-    })));
+describe('Started', () => {
+  var webServer = null;
+  beforeAll(() => {
+    webServer = new _web.default();
+  });
+  test('should start and trigger a callback', /*#__PURE__*/_asyncToGenerator(function* () {
+    var promise = webServer.start();
+    yield expect(promise).resolves.toBeUndefined();
+  }));
+  test('should stop and trigger a callback', /*#__PURE__*/_asyncToGenerator(function* () {
+    var promise = webServer.stop();
+    yield expect(promise).resolves.toBeUndefined();
+  }));
 });
